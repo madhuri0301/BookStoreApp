fd1b8435d7bbe661f480fa8d4b07047563aa6a54
Madhuri <harshupatil0301@gmail>
2021-09-23 16:32:52 +0530

    [Madhuri]Refactor: Added Book

--- src/app/Components/dialog-box/dialog-box.component.ts
+++ src/app/Components/dialog-box/dialog-box.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { BookService } from 'src/app/Services/book.service';
 
 @Component({
   selector: 'app-dialog-box',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogBoxComponent implements OnInit {
 
-  constructor() { }
+  booksArray: any;
+  card: any
+  bookId: any;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,private service: BookService,
+  private snackBar :MatSnackBar,private route:ActivatedRoute) { 
+    console.log("this is my card", data);
+  }
 
   ngOnInit(): void {
+  }
+  getBooks() {
+    this.service.getallBook('bookstore_user/get/book').subscribe((response: any) => {
+      console.log(response);
+      this.booksArray = response.result;
+      // this.booksArray.reverse();
+      console.log(this.booksArray);
+
+    });
   }
 
 }
